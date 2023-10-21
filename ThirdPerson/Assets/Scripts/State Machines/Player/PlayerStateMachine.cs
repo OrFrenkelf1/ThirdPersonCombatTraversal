a49fc2e4b32f0eef354d5f9d7269d179beb6e2d4
OrFrenkelf1 <or.frenkelf1@gmail.com>
2023-10-21 23:18:34 +0300

    More

--- ThirdPerson/Assets/Scripts/State Machines/Player/PlayerStateMachine.cs
+++ ThirdPerson/Assets/Scripts/State Machines/Player/PlayerStateMachine.cs
@@ -7,11 +7,16 @@ public class PlayerStateMachine : StateMachine
     [field:SerializeField] public InputReader InputReader { get; private set; }
     [field:SerializeField] public CharacterController Controller { get; private set; }
     [field: SerializeField] public Animator Animator { get; private set; }
-
     [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
+    [field: SerializeField] public float RotationDamping { get; private set; }
+
+    public Transform MainCameraTransform { get; private set; }
+
     void Start()
     {
-        SwitchState(new PlayerTestState(this));
+        MainCameraTransform = Camera.main.transform;
+
+        SwitchState(new PlayerFreeLookState(this));
     }
 
 }
